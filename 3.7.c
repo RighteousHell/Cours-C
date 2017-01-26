b1eb8636dea411d4e32b481a6466f1ec10ea1eb7
RighteousHell <RighteousHell@RIGHTEOUSHELLPC>
2017-01-26 15:48:13 +0300

    С 3.7 2

--- 3.7.c
+++ 3.7.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-
- struct TABLECELL {
+struct TABLECELL
+{
 	char ch;
 	int frequency;
-<<<<<<< HEAD
 };
-=======
-		  };
->>>>>>> origin/master
-
 void bubblesort(struct TABLECELL tc[], int size) {
 	for (int i = 0; i < size - 1; i++) {
 		bool swapped = false;
@@ -31,32 +26,31 @@ void bubblesort(struct TABLECELL tc[], int size) {
 	}
 }
 
-int main() {
-
+int main()
+{
 	printf("Enter string:\n");
-
 	char string[120];
-
 	gets(string);
-
 	struct TABLECELL firstcell[120];
 	struct TABLECELL* start = firstcell;
 	int tablesize = 0;
-
 	int size = strlen(string);
-
-	for (int i = 0; i<size; i++) {
+	for (int i = 0; i<size; i++)
+	{
 		struct TABLECELL* pnow = start;
 		bool newch = true;
-		for (int j = 0; j<tablesize; j++) {
+		for (int j = 0; j<tablesize; j++)
+		{
 			char comp = pnow->ch;
-			if (string[i] == comp) {
+			if (string[i] == comp)
+			{
 				pnow->frequency++;
 				newch = false;
 			}
 			pnow++;
 		}
-		if (newch == true) {
+		if (newch == true)
+		{
 			pnow = start;
 			for (int y = 0; y<tablesize; y++, pnow++);
 			pnow->ch = string[i];
@@ -64,19 +58,12 @@ int main() {
 			tablesize++;
 		}
 	}
-
 	bubblesort(firstcell, tablesize);
-
-	for (int k = 0; k<tablesize; k++) {
+	for (int k = 0; k<tablesize; k++)
+	{
 		printf("%c : %d\n", start->ch, start->frequency);
 		start++;
 	}
-
 	int end;
 	scanf("%d", &end);
-
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> origin/master
