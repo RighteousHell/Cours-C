7bd5fadd14b53eef34af66d4111bd8482f73666f
RighteousHell <Strahov.PG@gmail.com>
2017-02-16 10:49:51 +0300

    header

---
+++ 6.4./6.4.header.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+typedef long long LLI;
+
+int Traditional(int *a, int size);
+
+int Recurs(int *a, int size, int summ, int deep);
